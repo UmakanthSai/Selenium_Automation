fa8b1f77bc8d880d9278bc5bcfc545beda48af9e
umakanthsai <umakanthsaichebrolu@gmail.com>
2022-01-27 00:09:23 +0530

    added wedriver folder

---
+++ src/test/java/Automation/Selenium/sampleTest.java
@@ -0,0 +1,16 @@
+package Automation.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sampleTest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\c.umakanthsai\\Documents\\Projects\\Selenium_maven\\Selenium_Automation\\web_drivers\\chrome_97\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+
+	}
+
+}
